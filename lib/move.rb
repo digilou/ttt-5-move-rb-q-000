e56a7f5986eb81d9d6ec3c1ef5ee71cf7e6ccc16
Amy Carney <amy.carney@alaska.gov>
2017-12-13 17:45:42 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(array, index, value=" X ")
+def move(array, index, value="X")
   index = input_to_index
   array[index] = value
   array
