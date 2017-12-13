d965e22f5998cddd3111a2c5ff44c34124feac07
Amy Carney <amy.carney@alaska.gov>
2017-12-13 18:08:12 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,17 +7,11 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-# def input_to_index(input)
-#   input.to_i - 1
-# end
-#
-# def move(array, index, value="X")
-#   index = input_to_index
-#   array[index] = value
-#   array
-# end
-def move(board, index, type="X")
-  arr = board
-  arr[index.to_i-1] = type
-  arr
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(array, input, value="X")
+  array[input_to_index(input)] = value
+  array
 end
