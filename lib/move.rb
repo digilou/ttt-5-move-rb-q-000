8c54c24aaa72ee88d71d1df8c87dc2e91e99ae30
Amy Carney <amy.carney@alaska.gov>
2017-12-13 17:20:22 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,12 +7,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(input)
-  input.to_i
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
 def move(array, index, value)
   array = board
-  array[input_to_index - 1] = value
+  array[input_to_index] = value
   array
 end
